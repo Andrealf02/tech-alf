6782c941503c62a84c89458385d53587db222216
Andrea_f <web.andrea.02@gmail.com>
2022-09-26 10:31:29 +0200

    version 1.0
    -Conflict routes.

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+            DB::table('products')->insert([
+                'name'=>'Naranja',
+                'description'=>'Naranjas y cosas de naranjas',
+                'price'=> 0.20
+            ]);
+    }
+}
